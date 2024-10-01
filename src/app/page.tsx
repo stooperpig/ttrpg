ec0f3ce3bf4e8a320fa21a872e0d4cf700b8bd20
Bill <stooperpig@gmail.com>
2024-10-01 16:20:15 -0500

    escape aposta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <div>
       <br />Home<br />
       <p>Find Your Perfect Master for your Games
-        Looking for a compitent Game Master to bring your TTRPG campaign to life? Whethere you're into D&D, Pathfinder, or Call of Cthulhu, we've curated and hand picked a selction of expert Game Masters to run immersive games for you and your group.
+        Looking for a compitent Game Master to bring your TTRPG campaign to life? Whethere you&apos;re into D&D, Pathfinder, or Call of Cthulhu, we&apos;ve curated and hand picked a selction of expert Game Masters to run immersive games for you and your group.
         Choose Your Professional Adventure Guide
         - Handpicked DMs and GMs specializing in a variety of tabletop systems ranging from D&D to Forged in the Dark.
-        - Professional online virtual games experiences tailored to your group's style. 
+        - Professional online virtual games experiences tailored to your group&apos;s style. 
         - Dive into immersive virtual worlds from the comfort of you own home with a personalized guide.
 
         Why Play With Us?
@@ -14,7 +14,7 @@ export default function Home() {
         - Build a community with fellow gamers and Forever DMs to enjoy the best solution for remote play.
         - Explore various campaigns, from epic high fantasy to grungy cosmic horror, all led by experienced and trained professional GMs ready to craft unforgettable stories.
 
-        Whether you're a new player or a seasoned gamer, our platform is the perfect online platform to find a GM or DM to run your next adventure. Let us Master Your Games for you.
+        Whether you&apos;re a new player or a seasoned gamer, our platform is the perfect online platform to find a GM or DM to run your next adventure. Let us Master Your Games for you.
       </p>
     </div>
   );
