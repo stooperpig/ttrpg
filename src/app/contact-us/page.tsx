15bf7a6bf75239e3c1846c46291e676c7db8cc42
unknown <cgbaker4294@gmail.com>
2024-10-02 11:10:32 -0500

    Changes made to Contact Us Page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -2,7 +2,9 @@ export default function About() {
     return (
         <div>
             <br />Contact Us<br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore nesciunt corporis omnis, sit facilis totam fugit, amet labore commodi delectus beatae unde ab quisquam! Quas voluptates numquam hic dolorem!<br /><br />
+            <h1>Contact Us</h1>
+      <p>If you have any questions or comments, feel free to send us an email.</p>
+      <a href="mailto:cgbaker4294@gmail.com">Send Email</a>
         </div>
     )
 }
