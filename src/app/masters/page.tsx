8a716298a2d75d0905a4c892b9b8f67fdbc80c0f
unknown <stooperpig@gmail.com>
2024-10-02 17:09:55 -0500

    apos escapes

--- src/app/masters/page.tsx
+++ src/app/masters/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div>
             <br />Masters<br />
-            At Malve Studios, we take pride in handpicking each of our GMs. Every GM we bring on board has been carefully vetted to ensure they meet our high standards of creativity, professionalism, and passion for storytelling. We don&apos;t just find anyone to run your games—we curate GMs who understand the art of crafting unforgettable adventures. When you choose a GM through us, you're getting someone who&apos;s been personally selected to deliver a one-of-a-kind experience tailored to your group.<br /><br />
+            At Malve Studios, we take pride in handpicking each of our GMs. Every GM we bring on board has been carefully vetted to ensure they meet our high standards of creativity, professionalism, and passion for storytelling. We don&apos;t just find anyone to run your games—we curate GMs who understand the art of crafting unforgettable adventures. When you choose a GM through us, you&apos;re getting someone who&apos;s been personally selected to deliver a one-of-a-kind experience tailored to your group.<br /><br />
             <ul>
                 <li>Chuck</li>
                 <li>Mike</li>
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,9 +2,9 @@ export default function About() {
     return (
         <div>
             <br />Pricing<br />
-            At Malve Studios, we want to be super clear about our pricing for both drop-in games and long-term campaigns. Plus, we want you to know exactly how much your GM is earning from each session. For our Drop-In Campaigns, it's $40 per seat for a full 4-hour session. For long-term campaigns, it's $35 per seat for a 3-4 hour session, billed in 12-hour chunks.<br /><br />
+            At Malve Studios, we want to be super clear about our pricing for both drop-in games and long-term campaigns. Plus, we want you to know exactly how much your GM is earning from each session. For our Drop-In Campaigns, it&apos;s $40 per seat for a full 4-hour session. For long-term campaigns, it&apos;s $35 per seat for a 3-4 hour session, billed in 12-hour chunks.<br /><br />
 
-<br />Each GM gets $35 per seat for Drop-In Campaigns and $30 per seat for long-term campaigns. The GM and the group can decide the schedule for long-term campaigns, and you're free to skip sessions when needed. But if an entire 12-hour block is cancelled without a heads-up, we&apos;ll look into whether the campaign should continue or wrap up. If skipping a block is agreed upon in advance, no worries—there won&apos;t be any investigation.<br /><br />
+<br />Each GM gets $35 per seat for Drop-In Campaigns and $30 per seat for long-term campaigns. The GM and the group can decide the schedule for long-term campaigns, and you&apos;re free to skip sessions when needed. But if an entire 12-hour block is cancelled without a heads-up, weCll look into whether the campaign should continue or wrap up. If skipping a block is agreed upon in advance, no worries—there won&apos;t be any investigation.<br /><br />
 
 <br />Got any questions or concerns? Just reach out to us!<a href="/contact-us">Contact Us.</a><br /><br />
         </div>
