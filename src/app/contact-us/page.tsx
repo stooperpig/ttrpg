699f46ebc2f362ac3cdf4965d14f365538883553
unknown <stooperpig@gmail.com>
2024-10-02 12:15:16 -0500

    Edit to the Contact Us page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -3,8 +3,8 @@ export default function About() {
         <div>
             <br />Contact Us<br />
             <h1>Contact Us</h1>
-      <p>If you have any questions or comments, feel free to send us an email.</p>
-      <a href="mailto:cgbaker4294@gmail.com">Send Email</a>
+      <p>If you have any questions or comments, feel free to send us an email.
+      Please send emails to <a href="mailto:cgbaker4294@gmail.com">cgbaker4294@gmail.com</a></p>
         </div>
     )
 }
