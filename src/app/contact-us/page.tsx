1291a1f5e8d6c8b661df625631906ffd200ae2c1
unknown <stooperpig@gmail.com>
2024-10-02 17:10:47 -0500

    Removed header contact us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div>
             <br />Contact Us<br />
-            <h1>Contact Us</h1>
+            
       <p>If you have any questions or comments, feel free to send us an email.
       Please send emails to <a href="mailto:cgbaker4294@gmail.com">cgbaker4294@gmail.com</a></p>
         </div>
