daa4ce6021c24c66e72a42a9e2eb42e5b248f99e
unknown <stooperpig@gmail.com>
2024-10-02 12:09:10 -0500

    Test push with changes to contact us and pricing.

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div>
             <br />Pricing<br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore nesciunt corporis omnis, sit facilis totam fugit, amet labore commodi delectus beatae unde ab quisquam! Quas voluptates numquam hic dolorem!<br /><br />
+            We at Malve Studios want to make sure that we are crystal clear about pricing up front for both drop-in and long form campaigns. We also want to make sure you know exactly how much your Professional GM is making off of every session. Our pricing for our Drop-In Campaigns is $40/seat for the full 4 hour session. Our pricing for long form campaigns is $35/seat for a full 3-4 hour session and is billed in 12 hour blocks. Each Professional GM will make $35/seat for Drop-In Campaigns and $30/seat for long form campaigns for every session run. The GM and the party make their own schedule for long form campaigns. A GM or their party may skip sessions as wanted or necessary. However, should a full 12 hour block of sessions be cancelled consecutively without warning, there will be an investigation to determine if the campaign should close out without continuing or if a new block should be implemented. If the skipping of a block is agreed upon before the block is scheduled, there will be no investigation. For any concerns or questions, feel free to <a href="/contact-us">Contact Us.</a><br /><br />
         </div>
     )
 }
