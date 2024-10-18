7c16f0eb12a1c97d80402da3712f65840de62e04
unknown <stooperpig@gmail.com>
2024-10-18 13:04:19 -0500

    Changed email to business

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
             <br />Contact Us<br />
             
       <p>If you have any questions or comments, feel free to send us an email.
-      Please send emails to <a href="mailto:cgbaker4294@gmail.com">cgbaker4294@gmail.com</a></p>
+      Please send emails to <a href="mailto:malve.questions@malvestudios.com">malve.questions@malvestudios.com</a></p>
         </div>
     )
 }
