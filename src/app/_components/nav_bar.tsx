79c30abe9dd704b6c49404fc141acd62df0a8b19
Bill <stooperpig@gmail.com>
2024-10-23 18:27:30 -0500

    update to nav hover states

--- src/app/_components/nav_bar.tsx
+++ src/app/_components/nav_bar.tsx
@@ -6,17 +6,26 @@ import styles from './nav_bar.module.css';
 export default function NavBar() {
     const pathname = usePathname();  // Get the current pathname
 
+    const links = [
+        { name: 'Home', url: '/' },
+        { name: 'Masters', url: '/masters' },
+        { name: 'Services', url: '/services' },
+        { name: 'Reserve a GM', url: '/reserve' },
+        { name: 'Contact Us', url: '/contact-us' }
+    ];
+
     return (
         <nav className={styles.nav}>
             <ul className={styles.navList}>
-                {['Home', 'Masters', 'Services', 'Reserve a GM', 'Contact Us'].map((page, index) => {
-                    const pagePath = `/${page.toLowerCase() === 'home' ? '' : page.toLowerCase()}`;
+                {links.map((link, index) => {
+                    const pagePath = link.url;
                     const isActive = pathname === pagePath;
+                    console.log(`pathname: ${pathname} pagePath: ${pagePath}`);
 
                     return (
                         <li key={index} className={styles.navItem}>
                             <Link href={pagePath} className={`${styles.navLink} ${isActive ? styles.active : ''}`}>
-                                {page}
+                                {link.name}
                             </Link>
                         </li>
                     );
