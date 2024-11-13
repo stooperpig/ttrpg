2e1d39cfa7a0d429719eda8f38567af9135e9fd4
Bill <stooperpig@gmail.com>
2024-11-13 16:44:40 -0600

    move survey to new page with link from contact-us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,10 +1,14 @@
-import Survey from "../_components/survey/survey"
+import styles from './page.module.css';
+import Link from "next/link";
 
 export default function ContactUs() {
     return (
         <>
             <div className="top-section">
-                <Survey/>
+                List of links to different email addresses (based on subject matter)<br/>
+                or<br/>
+                contact form with inputs (including subject matter drop downs)<br/><br/>
+                <Link className={styles.link} href="/player-response">Text with link to player response form</Link>
             </div>
         </>
     )
