076309ea4157159f62952f396c20a233d4616e2d
unknown <stooperpig@gmail.com>
2024-11-23 22:29:39 -0600

    Added given code for Meta Pixel. Needs to be looked at.

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
             return (
                 <div className={styles.mobile_header_closed}>
                     <div className={styles.logo}>
-                    <Link href="/"> <Image src="/MS_Logo_White.svg" alt="My SVG" width={44} height={44} /> </Link>
+                        <Link href="/"> <Image src="/MS_Logo_White.svg" alt="My SVG" width={44} height={44} /> </Link>
                     </div>
                     <div className={styles.hamburger} onClick={toggleMobileNav}>
                         <img className={styles.my_svg} src="/MS_MenuButton.svg" alt="My SVG" width={44} height={44} />
@@ -63,12 +63,32 @@ export default function Header() {
             </div>
             <div className={styles.desktop_header}>
                 <div className={styles.logo}>
-                <Link href="/"> <Image src="/MS_Logo_White.svg" alt="My SVG" width={44} height={44} /> </Link>
+                    <Link href="/"> <Image src="/MS_Logo_White.svg" alt="My SVG" width={44} height={44} /> </Link>
                     <span className={styles.studio_logo_text}>MALVE STUDIOS</span>
                 </div>
                 <div className={styles.desktop_nav}><NavBar links={links} /></div>
             </div>
-
+            
+            
+            {/* //    <!-- Meta Pixel Code --> 
+            <script>
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+     s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '541454725373855');
+                fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=541454725373855&ev=PageView&noscript=1"
+            /></noscript>
+             <!-- End Meta Pixel Code -->  */}
         </div>
     )
+
+
 }
