6c5f1e9bc84c703504cbffcefe78d9e4eaae9267
unknown <stooperpig@gmail.com>
2025-02-05 12:02:08 -0600

    Update to description and metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,10 @@ import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Malve Studios",
-  description: "",
-  keywords: ["pro dm", "pro gm", "Professional Dungeon Master", "dungeon master", "dm", "game master"]
+  description: "Vetted professional DMs for online and in person Dungeons and Dragons games",
+  keywords: ["pro dm", "pro gm", "Professional Dungeon Master", "dungeon master", "dm", "game master",
+    "start playing dnd", "learn to play dnd", "dnd", "D&D", "dungeons and dragons", "ttrpg", "foundry",
+    "play dnd", "find dnd group", "how to find dnd groups near me", "play dnd online"]
 };
 
 export default function RootLayout({
@@ -20,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-6D0539ZP2P" strategy="beforeInteractive" />
-        <Script src="gtag.js" strategy="beforeInteractive"/>
+        <Script src="gtag.js" strategy="beforeInteractive" />
         <link rel="stylesheet" href="https://use.typekit.net/cix6ttg.css" />
         <Script src="pro_dm_fb_tag.js" strategy="beforeInteractive" />
       </head>
